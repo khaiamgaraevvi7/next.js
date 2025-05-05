9e45f093f40a8cc412ec9ad47960086e3793131b
Khaiam Garaevv <khaiamgaraevvi5@gmail.com>
2025-05-05 12:18:33 +0400

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-600 text-white text-2xl font-bold">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="bg-orange-500 h-64 w-124 rounded-lg">
-          <h2 className="text-center mt-28">Welcome Next.js (Demo mode)</h2>
+        <div className="">
+          <h2 className="text-center text-lg">Next.js</h2>
         </div>
+        <Button className="bg-blue-600 hover:bg-blue-800">Sipariş Edin</Button>
       </main>
     </div>
   );
