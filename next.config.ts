90b16ec3fa833f88cb46b1ddf182e9b7c0fb3941
Khaiam Garaevv <khaiamgaraevvi5@gmail.com>
2025-05-05 12:25:29 +0400

    Version 1.0.0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // Statik export üçün bu parametri əlavə edin
 };
 
 export default nextConfig;
