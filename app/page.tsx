4f0c4af09f542bc175d3a5404a53c86a5bf338ea
Khaiam Garaevv <khaiamgaraevvi1@gmail.com>
2025-05-05 13:07:57 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="">
           <h2 className="text-center text-lg">Next.js</h2>
-        </div>
-        <Button className="bg-blue-600 hover:bg-blue-800">Demo Mode</Button>
+          <Button className="bg-blue-600 hover:bg-blue-800">Demo Mode</Button>
       </main>
     </div>
   );
